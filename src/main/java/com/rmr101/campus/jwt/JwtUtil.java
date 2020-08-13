57dcb927e56f7a397cc3adeba4c2a018ade997fb
jackytsheng <48374842+jackytsheng@users.noreply.github.com>
2020-08-13 11:58:47 +1000

    Update JwtUtil.java

--- src/main/java/com/rmr101/campus/jwt/JwtUtil.java
+++ src/main/java/com/rmr101/campus/jwt/JwtUtil.java
@@ -43,7 +43,7 @@ public class JwtUtil {
     //the jwt will expire after 24 hours
     private String createToken(Map<String, Object> claims, String subject) {
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 10))
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
     }
 
